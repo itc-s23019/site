0b853510e8b8960bddb28afac2cb84bda07d0661
itc-s23019 <s23019@std.it-college.ac.jp>
2024-01-23 08:18:33 +0000

    2.6syou
    2.6syou finish

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,7 @@
-import Layout from 'components/layout'
 import Hero from 'components/hero'
 
 const Home = () => {
-  return (
-    <Layout>
-      <main>
-        <Hero />
-      </main>
-    </Layout>
-  )
+  return <Hero />
 }
 
 export default Home
